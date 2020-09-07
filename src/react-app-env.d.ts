ecfb6d5200cadc8ae535e9780408be3145cdbc55
Tobias Lindström <tobbelindstrom93@gmail.com>
2020-09-07 16:14:15 +0200

    Create OverwritableType type

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,18 @@
 /// <reference types="react-scripts" />
 
+// Redux
 interface Window {
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: Function;
 }
+
+// Overwritable type
+type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
+type Prefer<P, T> = P & Omit<T, keyof P>;
+type ElementPropsWithoutRef<T extends ReactType> = Pick<
+  ComponentPropsWithoutRef<T>,
+  keyof ComponentPropsWithoutRef<T>
+>;
+type OverwritableType<OwnProps, Type extends ReactType> = Prefer<
+  OwnProps,
+  ElementPropsWithoutRef<Type>
+>;
