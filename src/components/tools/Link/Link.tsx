e6df74458ec040510af983630b7ee7d7095f7513
Tobias Lindström <tobbelindstrom93@gmail.com>
2020-09-07 16:13:48 +0200

    Support external links

--- src/components/tools/Link/Link.tsx
+++ src/components/tools/Link/Link.tsx
@@ -8,17 +8,39 @@ import {
 
 interface LinkProps {
   children: ReactNode;
+  to?: string;
+  activeClassName?: string;
 }
 
 const Link = ({
   children,
+  activeClassName,
+  to,
   ...props
 }: LinkProps & RouterLinkLink & NavLinkProps) => {
-  if (props?.activeClassName) {
-    return <RouterNavLink {...props}>{children}</RouterNavLink>;
+  const internal = /^\/(?!\/)|^#/.test(to);
+
+  if (internal) {
+    if (activeClassName) {
+      return (
+        <RouterNavLink to={to} activeClassName={activeClassName} {...props}>
+          {children}
+        </RouterNavLink>
+      );
+    }
+
+    return (
+      <RouterLink to={to} {...props}>
+        {children}
+      </RouterLink>
+    );
   }
 
-  return <RouterLink {...props}>{children}</RouterLink>;
+  return (
+    <a href={to} {...props}>
+      {children}
+    </a>
+  );
 };
 
 export default Link;
